05947f94dfff2d3593a9ba1ea7fe8feef75a0947
Jamil RAICHOUNI <jamil.raichouni@deutschebahn.com>
2024-08-13 20:12:52 +0200

    feat: Add skeleton for rest-api addon project

---
+++ rest-api/src/Main.java
@@ -0,0 +1,32 @@
+import org.eclipse.ui.IStartup;
+
+public class Main implements IStartup {
+    /**
+     * Will be called in a separate thread after the workbench initializes.
+     * <p>
+     * Note that most workbench methods must be called in the UI thread since they
+     * may access SWT. For example, to obtain the current workbench window, use:
+     * </p>
+     *
+     * <pre>
+     * <code>
+     * IWorkbench workbench = PlatformUI.getWorkbench();
+     * workbench.getDisplay().asyncExec(new Runnable() {
+     *   public void run() {
+     *     IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+     *     if (window != null) {
+     *       // do something
+     *     }
+     *   }
+     * });
+     * </code>
+     * </pre>
+     *
+     * @see org.eclipse.swt.widgets.Display#asyncExec
+     * @see org.eclipse.swt.widgets.Display#syncExec
+     */
+    @Override
+    public void earlyStartup() {
+        System.out.println("earlyStartup() called");
+    }
+}
